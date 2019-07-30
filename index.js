aa8f1429a9633eeb80112ed609182acba57c42a2
hsuallan <33412402+hsuallan@users.noreply.github.com>
2019-07-30 15:00:08 +0800

    ver0.1.2 部分指令增加權限控制

--- index.js
+++ index.js
@@ -12,35 +12,45 @@ client.once('ready', () => {
 client.on('message', async msg => {
   const args = msg.content.slice(cfg.CmdPrefix.length).trim().split(/ +/g)
   const cmd = args.shift().toLowerCase()
+  const admin = msg.member.roles.has('424244648085815296');//true or false
   if(msg.author.bot) return;
   if (/gamemaps|workshop/.test(msg.content)&&args.length == 0) {
-    const {r,ans} = await db.is_reapeat(msg.content)
-    if(r){
-      msg.channel.send(`${ans.title} have been played`)
+    if(admin){
+      const {r,ans} = await db.is_reapeat(msg.content)
+      if(r){
+        msg.channel.send(`${ans.title} have been played`)
+      }
+      else{
+        try {
+          const data = await post(msg.content)
+          db.addMap(data).then(ans=>{
+            msg.channel.send(`${ans.title} have been saved`)
+          })
+        } catch (error) {
+          msg.channel.send(`${error}`)
+        }
+      }
     }
     else{
-      try {
-      const data = await post(msg.content)
-      db.addMap(data).then(ans=>{
-        msg.channel.send(`${ans.title} have been saved`)
-      })
-      } catch (error) {
-        msg.channel.send(`${error}`)
-      }
+      msg.channel.send(`Sorry you don't have Permission`)
     }
-    }
+  }
   if(cmd == 'finished'){
-    if(args.length == 0){
-      msg.channel.send(`This command have to at least one argument to work fine`) 
-    }
-    db.change_to_finish(args.join(' ').toString(),msg.createdAt).then((n)=>{
-      if(n ==0){
-        msg.channel.send(`Sorry Didn't find the map`) 
-      }else{
-        msg.channel.send(`${args.join(' ').toString()} is finished at ${msg.createdAt }`) 
+    if(admin){
+      if(args.length == 0){
+        msg.channel.send(`This command have to at least one argument to work fine`) 
       }
-      
-    })
+      db.change_to_finish(args.join(' ').toString(),msg.createdAt).then((n)=>{
+        if(n ==0){
+          msg.channel.send(`Sorry Didn't find the map`) 
+        }else{
+          msg.channel.send(`${args.join(' ').toString()} is finished at ${msg.createdAt }`) 
+        }
+      })
+    }
+    else{
+      msg.channel.send(`Sorry you don't have Permission`)
+    }
   }
   if(cmd == 'played'){
     let num = args.length  == 0 ? 0 : parseInt(args[0])
@@ -54,7 +64,7 @@ client.on('message', async msg => {
   if(cmd == 'now'){
     const data = await db.now()
     if(data === null){
-      msg.channel.send(`Playing queue is empty,maybe ypu can find some`)
+      msg.channel.send(`Playing queue is empty,maybe you can find some`)
     }
     else{
       msg.channel.send(`now playing ${data.url}`)
@@ -63,8 +73,8 @@ client.on('message', async msg => {
   if(cmd =='help'){
     msg.channel.send(
       `指令說明：
-        發送含有工作坊 or gamemaps的連結會自動加入追蹤
-        \`!finished mapname/url\` 將map標示為已完成
+        發送含有工作坊 or gamemaps的連結會自動加入追蹤(需admin權限)
+        \`!finished mapname/url\` 將map標示為已完成(需admin權限)
         \`!played (num)\` 發送遊玩過的列表，最近num筆
         \`!now \` 遊玩中的map
       `
